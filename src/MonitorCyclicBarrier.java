00b7326b1689bef8d020506529cb1565f49650a4
Kremerteam <47732635+Kremerteam@users.noreply.github.com>
2021-02-17 22:22:11 -0600

    Start on monitors

--- src/MonitorCyclicBarrier.java
+++ src/MonitorCyclicBarrier.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  aek2267
  cms6788
@@ -6,12 +8,26 @@
 
 public class MonitorCyclicBarrier {
 	
+	private int position;
+	ArrayList<Integer> buffer;
+	private int parties;
+	
 	public MonitorCyclicBarrier(int parties) {
+		buffer = new ArrayList<Integer>();
+		position=parties-1;
+		this.parties=parties;
 	}
 	
-	public int await() throws InterruptedException {
+	public int await() throws InterruptedException {//???
            int index = 0;
-		
+		   index=position;
+		   position--;
+		   buffer.add(position);
+           while(buffer.size()!=parties)
+        	   buffer.wait();
+           
+           buffer.notifyAll();
+           
           // you need to write this code
 	    return index;
 	}
