c2d7b8eba464d4f152cc1cc7fb64d716c52b24a5
Kremerteam <47732635+Kremerteam@users.noreply.github.com>
2021-02-24 22:05:58 -0600

    Fixes and test started

---
+++ src/PriorityQTest.java
@@ -0,0 +1,73 @@
+import java.util.concurrent.TimeUnit;
+
+public class PriorityQTest implements Runnable {
+	final static int SIZE = 10;
+	final static int ROUND = 5;
+	
+	/*final FairUnifanBathroom bathroom;
+	
+	public testFairUnifanBathroom(FairUnifanBathroom bathroom) {
+		this.bathroom = bathroom;
+	}
+	
+
+	public void run() {
+		double random = Math.random();
+		if(random < .5){
+			//System.out.println("OU");
+			bathroom.enterBathroomOU();
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//bathroom.leaveBathroomUT();
+			bathroom.leaveBathroomOU();
+		}else{
+		//	System.out.println("UT");
+			bathroom.enterBathroomUT();
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			bathroom.leaveBathroomUT();
+		}
+
+		
+	}
+	*/
+	public static void main(String[] args) {
+		PriorityQueue p = new PriorityQueue(5);
+		p.add("Hi", 9);
+		p.add("Yo", 6);
+		System.out.println(p.search("Hi"));
+		try {
+			System.out.println(p.getFirst());
+			System.out.println(p.getFirst());
+			System.out.println(p.search("Hi"));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//testFairUnifanBathroom test = new testFairUnifanBathroom(bathroom);
+		
+	/*	Thread[] t = new Thread[SIZE];
+		
+		for (int i = 0; i < SIZE; ++i) {
+			t[i] = new Thread(test);
+		}
+		
+		for (int i = 0; i < SIZE; ++i) {
+			t[i].start();
+		}*/
+    }
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,5 +1,6 @@
 
 import java.util.Stack;
+import java.util.Vector;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,12 +13,12 @@ public class PriorityQueue {
 	int maxSize;
 	int inQ;
 	int outQ;
-	String[] queue;
+	//String[] queue;
 	//int[] priority;
 	Condition notEmpty; 
 	Condition notFull;
-	Stack<String> stack;
-	Stack<Integer> Priorities;
+	Vector<String> queue;
+	Vector<Integer> Priorities;
 	
 	public PriorityQueue(int maxSize) {
         // Creates a Priority queue with maximum allowed size as capacity
@@ -25,10 +26,12 @@ public class PriorityQueue {
 		this.maxSize = maxSize;
 		notEmpty = lock.newCondition();
 		notFull = lock.newCondition();
-		queue = new String[Integer.MAX_VALUE];
-		//priority = new int[Integer.MAX_VALUE];
-		stack= new Stack<String>();
-		Priorities= new Stack<Integer>();
+		queue= new Vector<String>();
+		queue.setSize(maxSize);
+		Priorities= new Vector<Integer>();
+		Priorities.setSize(maxSize);
+		inQ=0;
+		
 
 	}
 
@@ -38,29 +41,38 @@ public class PriorityQueue {
         // otherwise, returns -1 if the name is already present in the list.
         // This method blocks when the list is full.
 		
-		//Priority 9 at end and priority 0 at beginning
-		if(stack.contains(name))
+		//Priority 0 at end and priority 9 at beginning
+		if(queue.contains(name))
 			return -1;
 		else if(Priorities.contains(priority))
 		{
-			int index = Priorities.indexOf(priority);
-			stack.insertElementAt(name, index);
+			int index = Priorities.lastIndexOf(priority)+1; //TODO check if full!!
+			queue.insertElementAt(name, index);
 			Priorities.insertElementAt(priority, index);
+			inQ++;
 			return index;
 		}
+		else if(inQ==0){
+			queue.set(0, name);
+			Priorities.set(0,priority);
+			inQ++;
+			return 0;
+		}
 		else {
-			for(int i=0;i<Priorities.size();i++)
+			for(int i=0;i<=inQ;i++)
 			{
-				if(Priorities.get(i)>=priority || i==Priorities.size()-1)
+				if(i==inQ || Priorities.get(i)<priority)
 				{
 					int index=i;
-					stack.insertElementAt(name, i);
+					queue.insertElementAt(name, i);
 					Priorities.insertElementAt(priority, i);
+					inQ++;
 					return index;
 				}
 			}
-			
 		}
+			
+
 		return -2;
 	}
 
@@ -68,8 +80,8 @@ public class PriorityQueue {
         // Returns the position of the name in the list;
         // otherwise, returns -1 if the name is not found.
 		
-		if(stack.contains(name))
-			return stack.indexOf(name);
+		if(queue.contains(name))
+			return queue.indexOf(name);
 		else
 			return -1;
 	}
@@ -79,10 +91,9 @@ public class PriorityQueue {
         // or blocks the thread if the list is empty.
 
 		try {
-			while(inQ==0)
-				notEmpty.await();
-			
-			return stack.pop();
+			//while(inQ==0)
+				//notEmpty.await();
+			return queue.remove(0);
 		} finally {
 			
 		}
