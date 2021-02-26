8e8e928485d27bb86dc68a4be90b3a88f826268a
Kremerteam <47732635+Kremerteam@users.noreply.github.com>
2021-02-26 15:17:16 -0600

    LinkedList

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,4 +1,5 @@
 
+import java.util.LinkedList;
 import java.util.Vector;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -14,8 +15,8 @@ public class PriorityQueue {
 	int outQ;
 	Condition notEmpty; 
 	Condition notFull;
-	Vector<String> queue;
-	Vector<Integer> Priorities;
+	LinkedList<String> queue;
+	LinkedList<Integer> Priorities;
 	
 	public PriorityQueue(int maxSize) {
         // Creates a Priority queue with maximum allowed size as capacity
@@ -23,10 +24,8 @@ public class PriorityQueue {
 		this.maxSize = maxSize;
 		notEmpty = reLock.newCondition();
 		notFull = reLock.newCondition();
-		queue= new Vector<String>();
-		queue.setSize(maxSize);
-		Priorities= new Vector<Integer>();
-		Priorities.setSize(maxSize);
+		queue= new LinkedList<String>();
+		Priorities= new LinkedList<Integer>();
 		inQ=0;
 		
 	}
@@ -48,16 +47,14 @@ public class PriorityQueue {
 		else if(Priorities.contains(priority))
 		{
 			int index = Priorities.lastIndexOf(priority)+1;
-			queue.insertElementAt(name, index);
-			queue.setSize(maxSize);
-			Priorities.insertElementAt(priority, index);
-			Priorities.setSize(maxSize);
+			queue.add(index, name);
+			Priorities.add(index, priority);
 			inQ++;
 			return index;
 		}
 		else if(inQ==0){
-			queue.set(0, name);
-			Priorities.set(0,priority);
+			queue.add(name);
+			Priorities.add(priority);
 			inQ++;
 			return 0;
 		}
@@ -67,10 +64,8 @@ public class PriorityQueue {
 				if(i==inQ || Priorities.get(i)<priority)
 				{
 					int index=i;
-					queue.insertElementAt(name, i);
-					queue.setSize(maxSize);
-					Priorities.insertElementAt(priority, i);
-					Priorities.setSize(maxSize);
+					queue.add(i, name);
+					Priorities.add(i, priority);
 					inQ++;
 					return index;
 				}
@@ -110,10 +105,8 @@ public class PriorityQueue {
 			if(inQ==0)
 				notEmpty.await();
 			Priorities.remove(0);
-			Priorities.setSize(maxSize);
 			inQ--;
 			String result = queue.remove(0);
-			queue.setSize(maxSize);
 			return result;
 		} finally {
 
